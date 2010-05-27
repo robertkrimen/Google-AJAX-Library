6f5541ff5ae647df21eb84c0c8c0b273cb0cdfb3
robertkrimen <robertkrimen@gmail.com>
2010-05-26 18:37:15 -0700

    Convert to Dzpl

--- lib/Google/AJAX/Library.pm
+++ lib/Google/AJAX/Library.pm
@@ -1,19 +1,8 @@
 package Google::AJAX::Library;
+# ABSTRACT: Access the Google AJAX Libaries API in Perl
 
 use warnings;
 use strict;
-
-=head1 NAME
-
-Google::AJAX::Library - Access the Google AJAX Libaries API in Perl
-
-=head1 VERSION
-
-Version 0.02
-
-=cut
-
-our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
@@ -78,7 +67,7 @@ has file => qw/is ro/;
 has name => qw/is ro/;
 has ua => qw/is ro required 1 lazy 1 isa LWP::UserAgent/, default => sub {
     my $ua = LWP::UserAgent->new;
-    $ua->agent("Google::AJAX::Library/$VERSION (" . $ua->agent . ")");
+    $ua->agent("Google::AJAX::Library/$Google::AJAX::Library::VERSION (" . $ua->agent . ")");
     return $ua;
 };
 
@@ -364,10 +353,6 @@ sub _christen {
 
 __PACKAGE__->meta->make_immutable;
 
-=head1 AUTHOR
-
-Robert Krimen, C<< <rkrimen at cpan.org> >>
-
 =head1 SEE ALSO
 
 L<http://code.google.com/apis/ajaxlibs/>
@@ -376,56 +361,6 @@ L<JS::jQuery::Loader>
 
 L<JS::YUI::Loader>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-google-ajax-library at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Google-AJAX-Library>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Google::AJAX::Library
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Google-AJAX-Library>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Google-AJAX-Library>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Google-AJAX-Library>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Google-AJAX-Library>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008 Robert Krimen
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
 =cut
 
-1; # End of Google::AJAX::Library
+1;
